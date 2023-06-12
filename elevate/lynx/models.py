bbc8c1d0d4eb00fea6602b8dc38931ba379e3d9b
demetre <deme.barbakadze2@gmail.com>
2023-06-12 11:03:36 +0400

    see posts

---
+++ elevate/lynx/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Article(models.Model):
+    
+    title = models.CharField(max_length=255)
+    
+    description = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        
+        verbose_name_plural = 'Blog posts' 
